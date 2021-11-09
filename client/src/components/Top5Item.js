27c99a0a1230f48540cadbe95c905c670a409188
George <george@Georges-MacBook-Pro.local>
2021-11-08 21:03:19 -0500

    ItemEdit

--- client/src/components/Top5Item.js
+++ client/src/components/Top5Item.js
@@ -15,6 +15,8 @@ function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
     const [editActive, setEditActive] = useState(false);
     const [draggedTo, setDraggedTo] = useState(0);
+    const [ text, setText ] = useState("");
+    const { idNamePair, selected } = props;
 
     function handleDragStart(event, targetId) {
         event.dataTransfer.setData("item", targetId);
@@ -48,47 +50,94 @@ function Top5Item(props) {
         store.addMoveItemTransaction(sourceId, targetId);
     }
 
+    function handleToggleEdit(event) {
+        event.stopPropagation();
+        toggleEdit();
+    }
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {
+            store.setIsItemEditActive();
+        }
+        setEditActive(newActive);
+    }
+    let editStatus = false;
+    if (store.isItemEditActive) {
+        editStatus = true;
+    }
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            let index = event.target.id.substring("list-".length);
+            let text = event.target.value;
+            store.addUpdateItemTransaction(index-1, text);
+            toggleEdit();
+        }
+    }
+    function handleUpdateText(event) {
+        setText(event.target.value );
+    }
     let { index } = props;
 
     let itemClass = "top5-item";
     if (draggedTo) {
         itemClass = "top5-item-dragged-to";
     }
-
-    return (
-            <ListItem
-                id={'item-' + (index+1)}
-                key={props.key}
+let itemElement =<ListItem
+id={'item-' + (index+1)}
+key={props.key}
+className={itemClass}
+onDragStart={(event) => {
+    handleDragStart(event, (index+1))
+}}
+onDragOver={(event) => {
+    handleDragOver(event, (index+1))
+}}
+onDragEnter={(event) => {
+    handleDragEnter(event, (index+1))
+}}
+onDragLeave={(event) => {
+    handleDragLeave(event, (index+1))
+}}
+onDrop={(event) => {
+    handleDrop(event, (index+1))
+}}
+draggable="true"
+sx={{ display: 'flex', p: 1 }}
+style={{
+    fontSize: '48pt',
+    width: '100%'
+}}
+>
+<Box sx={{ p: 1 }}>
+<IconButton aria-label='edit'>
+    <EditIcon style={{fontSize:'48pt'}}
+    onClick={handleToggleEdit}  />
+    
+</IconButton>
+</Box>
+<Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
+</ListItem>;
+    if (editActive) {
+        itemElement =
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id={'item-' +(index+1)}
+                label="Top 5 List Name"
+                name="name"
+                autoComplete="Top 5 List Name"
                 className={itemClass}
-                onDragStart={(event) => {
-                    handleDragStart(event, (index+1))
-                }}
-                onDragOver={(event) => {
-                    handleDragOver(event, (index+1))
-                }}
-                onDragEnter={(event) => {
-                    handleDragEnter(event, (index+1))
-                }}
-                onDragLeave={(event) => {
-                    handleDragLeave(event, (index+1))
-                }}
-                onDrop={(event) => {
-                    handleDrop(event, (index+1))
-                }}
-                draggable="true"
-                sx={{ display: 'flex', p: 1 }}
-                style={{
-                    fontSize: '48pt',
-                    width: '100%'
-                }}
-            >
-            <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
-                    <EditIcon style={{fontSize:'48pt'}}  />
-                </IconButton>
-            </Box>
-                <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
-            </ListItem>
+                onKeyPress={handleKeyPress}
+                onChange={handleUpdateText}
+                defaultValue={props.name}
+                inputProps={{style: {fontSize: 48}}}
+                InputLabelProps={{style: {fontSize: 24}}}
+                autoFocus
+            />
+    }
+    return (
+        itemElement
     )
 }
 
